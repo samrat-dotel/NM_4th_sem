cbbed447fa2ced2a62c66c7a680a2540c7c8a816
samrat-dotel <samratdotel7264@gmail.com>
2024-01-22 09:25:33 +0545

    solution for bisection methos

---
+++ BisectionMethod.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+#define ERROR 0.0001
+
+double function(float x){
+    return (pow(x,3)-6*x+2);
+}
+
+int main(){
+    float guess1, guess2;
+    printf("Enter the first and second guesses:");
+    scanf("%f%f", &guess1, &guess2);
+
+    if (function(guess1)*function(guess2) >= 0){
+        printf("Invalid guesses.");
+        exit(1);
+    }
+
+    float random;
+    double err;
+
+    do{
+        printf("%f\t\t%f\t\t", guess1, guess2);
+
+        random = (guess1 + guess2)/2;
+        printf("%f\n", random);
+
+        if (function(guess2) * function(random) < 0 ){
+            guess1 = random;
+        }
+        else{
+            guess2 = random;
+        }
+
+        err = fabs((guess2 - guess1) / guess2);
+    }while(err > ERROR);
+}
